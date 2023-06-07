00ea2c78409472af037088596ee79be3d5d5584c
JoseLara55 <106717577+JoseLara55@users.noreply.github.com>
2023-06-07 10:49:35 +0000

    Coreccion😛

--- server/domains/user/user.validator.js
+++ server/domains/user/user.validator.js
@@ -3,18 +3,26 @@ import * as Yup from 'yup';
 
 // Creando un esquema de validación para el proyecto
 const userSchema = Yup.object().shape({
-    name: Yup.string().required('Se requiere un Correo'),
-    password: Yup.string().min(6).required('Se requiere ingresar password de al menos 6 caracteres')
+    name: Yup.string().required,
+    password: Yup.string().min(6).required
 });
 
 // Creando el extractor de datos de la petición
 const getUser = (req) => {
     // Extrayendo datos de la petición
-    const {correo} = req.body;
+    const {correo,password,nombre,fecha,entd,CURP,RFC,numero,configpass,apellidos} = req.body;
     // Regresando el objeto proyecto
     return {
         correo,
-        password
+        password,
+        nombre,
+        fecha,
+        entd,
+        CURP,
+        RFC,
+        numero,
+        configpass,
+        apellidos
     };
   };
 
